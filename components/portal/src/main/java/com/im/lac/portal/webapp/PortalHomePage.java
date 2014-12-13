c6277a284b3eb9bc11d68532e83622730bf9ae6c
gustavosantucho <gustavosantucho@gmail.com>
2014-12-13 18:54:04 -0300

    Added quick tree table component to home page

--- components/portal/src/main/java/com/im/lac/portal/webapp/PortalHomePage.java
+++ components/portal/src/main/java/com/im/lac/portal/webapp/PortalHomePage.java
@@ -1,9 +1,17 @@
 package com.im.lac.portal.webapp;
 
 import com.im.lac.wicket.semantic.NotifierProvider;
+import com.im.lac.wicket.semantic.SemanticResourceReference;
+import com.inmethod.grid.common.AbstractGrid;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.settings.def.ApplicationSettings;
 
 import javax.inject.Inject;
 
@@ -16,6 +24,18 @@ public class PortalHomePage extends WebPage {
         notifierProvider.createNotifier(this, "notifier");
         add(new MenuPanel("menuPanel"));
         addShowNotifierAction();
+        addVisualizerPanel();
+    }
+
+    @Override
+    public void renderHead(HtmlHeaderContainer container) {
+        super.renderHead(container);
+        IHeaderResponse response = container.getHeaderResponse();
+        response.render(JavaScriptHeaderItem.forReference(SemanticResourceReference.get()));
+        response.render(CssHeaderItem.forReference(new CssResourceReference(AbstractGrid.class, "res/style.css")));
+        response.render(CssHeaderItem.forReference(new CssResourceReference(ApplicationSettings.class, "res/select2.css")));
+        response.render(CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/semantic-overrides.css")));
+        response.render(CssHeaderItem.forReference(new CssResourceReference(SemanticResourceReference.class, "resources/easygrid-overrides.css")));
     }
 
     private void addShowNotifierAction() {
@@ -26,5 +46,9 @@ public class PortalHomePage extends WebPage {
                 notifierProvider.getNotifier(PortalHomePage.this).notify("Title", "Some message here");
             }
         });
+    }
+
+    private void addVisualizerPanel() {
+        add(new VisualizerPanel("visualizerPanel"));
     }
 }
---
+++ components/portal/src/main/java/com/im/lac/portal/webapp/VisualizerPanel.java
@@ -0,0 +1,72 @@
+package com.im.lac.portal.webapp;
+
+import com.inmethod.grid.IGridColumn;
+import com.inmethod.grid.treegrid.TreeGrid;
+import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisualizerPanel extends Panel {
+
+    public VisualizerPanel(String id) {
+        super(id);
+        addTreeTable();
+    }
+
+    @Override
+    public void renderHead(HtmlHeaderContainer container) {
+    }
+
+    private void addTreeTable() {
+        DefaultTreeModel treeGridModel = createTreeGridModel();
+        List<IGridColumn<DefaultTreeModel, DefaultMutableTreeNode, String>> columns = createTreeTableColumns();
+        TreeGrid<DefaultTreeModel, DefaultMutableTreeNode, String> treeGrid = new TreeGrid<DefaultTreeModel, DefaultMutableTreeNode, String>("treeTable", treeGridModel, columns);
+        add(treeGrid);
+    }
+
+    private List<IGridColumn<DefaultTreeModel, DefaultMutableTreeNode, String>> createTreeTableColumns() {
+        List<IGridColumn<DefaultTreeModel, DefaultMutableTreeNode, String>> columns;
+        columns = new ArrayList<IGridColumn<DefaultTreeModel, DefaultMutableTreeNode, String>>();
+        return columns;
+    }
+
+    private DefaultTreeModel createTreeGridModel() {
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode(new TreeColumnObject("root"));
+        DefaultTreeModel model = new DefaultTreeModel(rootNode);
+        DefaultMutableTreeNode rootChild1Node = new DefaultMutableTreeNode(new TreeColumnObject("Structure1"));
+        rootNode.add(rootChild1Node);
+        DefaultMutableTreeNode rootChild2Node = new DefaultMutableTreeNode(new TreeColumnObject("Structure2"));
+        rootNode.add(rootChild2Node);
+
+        rootChild1Node.add(new DefaultMutableTreeNode(new TreeColumnObject("child1 of Structure1")));
+        rootChild1Node.add(new DefaultMutableTreeNode(new TreeColumnObject("child2 of Structure1")));
+
+        rootChild2Node.add(new DefaultMutableTreeNode(new TreeColumnObject("child1 of Structure2")));
+        rootChild2Node.add(new DefaultMutableTreeNode(new TreeColumnObject("child2 of Structure2")));
+
+        return model;
+    }
+
+    private class TreeColumnObject implements Serializable {
+
+        private String description;
+
+        private TreeColumnObject(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
+}
