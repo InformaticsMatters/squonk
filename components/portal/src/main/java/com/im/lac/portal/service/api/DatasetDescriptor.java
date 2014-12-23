d1f6b6d2083209402324855e2bee1905be7e30f7
gustavosantucho <gustavosantucho@gmail.com>
2014-12-23 00:47:09 -0300

    Repackaged

--- components/portal/src/main/java/com/im/lac/portal/service/api/DatasetDescriptor.java
+++ components/portal/src/main/java/com/im/lac/portal/service/api/DatasetDescriptor.java
@@ -1,11 +1,13 @@
 package com.im.lac.portal.service.api;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class DatasetDescriptor implements Serializable {
 
     private Long datasetId;
     private String description;
+    private List<DatasetRowDescriptor> datasetRowDescriptorList;
 
     public Long getDatasetId() {
         return datasetId;
