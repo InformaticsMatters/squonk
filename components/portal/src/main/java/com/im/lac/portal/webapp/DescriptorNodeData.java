29e0b36b5c1575fb167350d4e7e129b9486d1875
santiagosantucho <santiagosantucho@gmail.com>
2015-01-07 15:27:08 -0300

    More on lac-19.

---
+++ components/portal/src/main/java/com/im/lac/portal/webapp/DescriptorNodeData.java
@@ -0,0 +1,28 @@
+package com.im.lac.portal.webapp;
+
+import com.im.lac.portal.service.api.DatasetDescriptor;
+import com.im.lac.portal.service.api.PropertyDescriptor;
+import com.im.lac.portal.service.api.RowDescriptor;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class DescriptorNodeData extends DefaultMutableTreeNode {
+
+    private DatasetDescriptor datasetDescriptor;
+    private RowDescriptor rowDescriptor;
+    private PropertyDescriptor propertyDescriptor;
+    private DescriptorType descriptorType;
+
+    public String getDescription() {
+        String description = null;
+        if (descriptorType.equals(DescriptorType.DATASET)) {
+            description = datasetDescriptor.getDescription();
+        } else if (descriptorType.equals(DescriptorType.ROW)) {
+            description = rowDescriptor.getDescription();
+        } else if (descriptorType.equals(DescriptorType.PROPERTY)) {
+            description = propertyDescriptor.getDescription();
+        }
+        return description;
+    }
+
+}
---
+++ components/portal/src/main/java/com/im/lac/portal/webapp/DescriptorType.java
@@ -0,0 +1,9 @@
+package com.im.lac.portal.webapp;
+
+public enum DescriptorType {
+
+    DATASET,
+    ROW,
+    PROPERTY
+
+}
