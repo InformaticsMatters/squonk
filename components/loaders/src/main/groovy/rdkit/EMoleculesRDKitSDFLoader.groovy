5db00f5dd78d3955a455ec6ab5ccd87af2587207
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2015-11-18 11:07:18 +0000

    exact search and logging improvements to RDKit search

---
+++ components/loaders/src/main/groovy/rdkit/EMoleculesRDKitSDFLoader.groovy
@@ -0,0 +1,39 @@
+package rdkit
+
+
+import com.squonk.rdkit.db.*
+import groovy.transform.InheritConstructors
+import javax.sql.DataSource
+
+/**
+ *
+ * @author timbo
+ */
+@InheritConstructors
+class EMoleculesRDKitSDFLoader extends AbstractRDKitSDFLoader {
+    
+
+    static void main(String[] args) {
+        
+        ConfigObject props = Utils.createConfig(new File('rdkit_loader.properties').toURL())
+        String baseTable = props.emolecules.table
+        String schema = props.database.schema
+        Map<String,Class> propertyToTypeMappings = props.emolecules.fields
+        DataSource dataSource = Utils.createDataSource(props.database, props.database.username, props.database.password)
+        Map<String,String> extraColumnDefs = props.emolecules.extraColumnDefs
+        String file = props.emolecules.path + '/' + props.emolecules.file
+        
+        EMoleculesRDKitSDFLoader loader = new EMoleculesRDKitSDFLoader(dataSource, schema, baseTable, RDKitTable.MolSourceType.CTAB, extraColumnDefs)
+        
+        loader.load(file, propertyToTypeMappings, [
+                RDKitTable.FingerprintType.RDKIT, 
+                RDKitTable.FingerprintType.MORGAN_CONNECTIVITY_2, 
+                RDKitTable.FingerprintType.MORGAN_FEATURE_2
+            ],
+        10000)
+        
+        loader.testSearcher()
+    }
+   
+}
+
