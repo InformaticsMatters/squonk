e15e8204cf61d0ef579a1080b4b3b69a7ef77b08
Tim Dudgeon <tudgeon@informaticsmatters.com>
2015-11-29 16:34:14 +0000

    docker setup for notebook

---
+++ docker/notebook_portal/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HOME=$PWD
+echo HOME = $HOME
+
+rm *.war
+
+cd ../../components; ./gradlew core-services/core-services-notebook:build
+cd $HOME
+cp ../../components/core-services/core-services-notebook/build/libs/core-services/core-services-notebook-0.2-SNAPSHOT.war notebook.war
+
+cd ../../../portal/portal-app; ant -f portal.xml build-portal-app-zip
+cd $HOME
+cp ../../../portal/portal-app/dist/portal-app.war ROOT.war
+
