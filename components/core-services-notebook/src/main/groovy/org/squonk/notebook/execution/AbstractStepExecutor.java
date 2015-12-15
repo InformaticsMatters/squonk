2c13732c1a157bddded8703c7b88bc3a60896826
Tim Dudgeon <tudgeon@informaticsmatters.com>
2015-12-15 12:05:57 +0000

    options should be optional

--- components/core-services-notebook/src/main/groovy/org/squonk/notebook/execution/AbstractStepExecutor.java
+++ components/core-services-notebook/src/main/groovy/org/squonk/notebook/execution/AbstractStepExecutor.java
@@ -3,24 +3,30 @@ package org.squonk.notebook.execution;
 import org.squonk.execution.steps.StepDefinition;
 import org.squonk.notebook.api.CellDTO;
 import org.squonk.notebook.api.CellType;
+import org.squonk.notebook.api.OptionDTO;
 import org.squonk.notebook.client.CallbackClient;
 import org.squonk.execution.steps.StepExecutor;
 import org.squonk.execution.variable.impl.CellCallbackClientVariableLoader;
 import org.squonk.execution.variable.VariableManager;
+
 import javax.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
- *
  * @author timbo
  */
 public abstract class AbstractStepExecutor implements QndCellExecutor {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStepExecutor.class.getName());
+
     @Inject
     protected CallbackClient callbackClient;
-    
+
     protected final String cellTypeName;
-    
-    
+
+
     public AbstractStepExecutor(String cellTypeName) {
         this.cellTypeName = cellTypeName;
     }
@@ -29,7 +35,7 @@ public abstract class AbstractStepExecutor implements QndCellExecutor {
 
     @Override
     public boolean handles(CellType cellType) {
-        System.out.println("Testing cell type name " + cellType.getName() + " for " + cellTypeName);
+        LOG.info("Testing cell type name " + cellType.getName() + " for " + cellTypeName);
         return cellType.getName().equals(cellTypeName);
     }
 
@@ -49,7 +55,16 @@ public abstract class AbstractStepExecutor implements QndCellExecutor {
     }
 
     protected StepDefinition configureOption(StepDefinition step, CellDTO cell, String option) {
-        return step.withOption(option, cell.getOptionMap().get(option).getValue());
+        OptionDTO dto = cell.getOptionMap().get(option);
+        if (dto == null) {
+            if (LOG.isLoggable(Level.INFO)) {
+                String values = cell.getOptionMap().keySet().stream().collect(Collectors.joining(","));
+                LOG.info("Option " + option + " not found. Possible values are " + values);
+            }
+            return step;
+        } else {
+            return step.withOption(option, dto.getValue());
+        }
     }
 
 }
