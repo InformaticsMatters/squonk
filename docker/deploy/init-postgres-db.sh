8ad291aee47339cf5c790ef2f4cb721a185ba633
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2016-02-17 14:13:55 +0000

    added SSL setup for keycloak

--- docker/deploy/init-postgres-db.sh
+++ docker/deploy/init-postgres-db.sh
@@ -4,8 +4,11 @@ PG_USER=${POSTGRES_USER:-postgres}
 echo "Setting up db as user ${PG_USER}"
 
 psql --username "$PG_USER" --command "CREATE USER keycloak WITH PASSWORD '${POSTGRES_KEYCLOAK_PASS:-squonk}';"
+echo "created keycloak user with password ${POSTGRES_KEYCLOAK_PASS:-squonk}"
 psql --username "$PG_USER" --command "CREATE USER xwiki WITH PASSWORD '${POSTGRES_XWIKI_PASS:-squonk}';"
+echo "created xwiki user with password ${POSTGRES_XWIKI_PASS:-squonk}"
 psql --username "$PG_USER" --command "CREATE USER squonk WITH PASSWORD '${POSTGRES_SQUONK_PASS:-squonk}';"
+echo "created squonk user with password ${POSTGRES_SQUONK_PASS:-squonk}"
 
 createdb --username "$PG_USER" -O $PG_USER squonk
 createdb --username "$PG_USER" -O keycloak keycloak
