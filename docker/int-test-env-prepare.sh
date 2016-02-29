cb9f02089597fceec89aff6c34cd365a4e3953c9
Tim Dudgeon <tudgeon@informaticsmatters.com>
2016-02-29 18:19:24 +0000

    localhost as default

--- docker/int-test-env-prepare.sh
+++ docker/int-test-env-prepare.sh
@@ -7,7 +7,7 @@ docker-compose up -d
 
 # we need to wait for postgres to start as the next step is to populate the database
 attempt=0
-until nc -z $SQUONK_DB_SERVER 5432
+until nc -z ${SQUONK_DB_SERVER:-localhost} 5432
 do
     if [ $attempt -gt 10 ]; then 
         echo "Giving up on postgres"
