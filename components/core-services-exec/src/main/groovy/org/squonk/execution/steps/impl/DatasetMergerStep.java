5e4a108b8216665f276b7b8ae894c0795f2ab3e5
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2016-08-26 13:04:43 +0100

    extended metadata for datset merger

--- components/core-services-exec/src/main/groovy/org/squonk/execution/steps/impl/DatasetMergerStep.java
+++ components/core-services-exec/src/main/groovy/org/squonk/execution/steps/impl/DatasetMergerStep.java
@@ -1,17 +1,19 @@
 package org.squonk.execution.steps.impl;
 
+import org.squonk.dataset.DatasetMetadata;
 import org.squonk.execution.steps.AbstractStep;
 import org.squonk.execution.steps.StepDefinitionConstants;
 import org.squonk.execution.variable.VariableManager;
 import org.squonk.types.BasicObject;
 import org.squonk.dataset.Dataset;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.camel.CamelContext;
+import org.squonk.types.io.JsonHandler;
 
 /**
  *
@@ -33,6 +35,8 @@ public class DatasetMergerStep extends AbstractStep {
 
     public static final String VAR_OUTPUT = StepDefinitionConstants.VARIABLE_OUTPUT_DATASET;
 
+    private static final String SOURCE = "Squonk Dataset merger";
+
     @Override
     public void execute(VariableManager varman, CamelContext context) throws Exception {
         String mergeField = getOption(OPTION_MERGE_FIELD_NAME, String.class);
@@ -41,13 +45,40 @@ public class DatasetMergerStep extends AbstractStep {
         LOG.log(Level.INFO, "Merging using field {0}, keep first value={1}", new Object[]{mergeField, keepFirst});
 
         Map<Object, BasicObject> results = new LinkedHashMap<>();
+        DatasetMetadata<? extends BasicObject> meta = null;
+
         Class type = null;
+        int count = 0;
+        List<String> sources = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             Dataset<? extends BasicObject> nextDataset = fetchMappedInput(VAR_INPUT_BASE + i, Dataset.class, varman);
-            if (type == null) {
-                type = nextDataset.getType();
-            }
+
             if (nextDataset != null) {
+                count++;
+                DatasetMetadata<? extends BasicObject> oldMeta = nextDataset.getMetadata();
+                Object source = oldMeta.getProperties().get(DatasetMetadata.PROP_SOURCE);
+                sources.add(source == null ? "undefined source" : source.toString());
+                if (type == null) {
+                    type = nextDataset.getType();
+                    meta = new DatasetMetadata(type);
+                    meta.getProperties().put(DatasetMetadata.PROP_CREATED, DatasetMetadata.now());
+                    meta.getProperties().put(DatasetMetadata.PROP_SOURCE, SOURCE);
+                }
+
+
+                for (DatasetMetadata.PropertiesHolder ph : oldMeta.getFieldMetaProps()) {
+                    String fldName = ph.getFieldName();
+                    if (meta.getFieldMetaProp(fldName, DatasetMetadata.PROP_CREATED) == null) {
+                        if (fldName.equals(mergeField)) {
+                            meta.createField(fldName, SOURCE, "Common key used for merging", null);
+                            meta.appendFieldHistory(fldName, "Values defined from merge key");
+                        } else {
+                            meta.createField(fldName, SOURCE, "Values merged from multiple datasets", null);
+                            meta.appendFieldHistory(fldName, "Values merged from multiple datasets");
+                        }
+                    }
+                }
+
                 statusMessage = "Handling dataset " + i;
                 LOG.log(Level.INFO, "Handling dataset {0}", i);
                 Stream<? extends BasicObject> st = nextDataset.getStream();
@@ -68,15 +99,17 @@ public class DatasetMergerStep extends AbstractStep {
                 });
                 st.close();
 
-                // TODO - merge the metadata?
             }
         }
 
+        meta.getProperties().put(DatasetMetadata.PROP_DESCRIPTION, "Merged from " + count + " datasets using field " + mergeField
+        + ". Sources were: " + sources.stream().collect(Collectors.joining(", ")));
+
         if (type != null) {
-            Dataset output = new Dataset<>(type, results.values());
+            Dataset output = new Dataset(type, results.values(), meta);
             createMappedOutput(VAR_OUTPUT, Dataset.class, output, varman);
             statusMessage = String.format(MSG_RECORDS_PROCESSED, output.getMetadata().getSize());
-            LOG.info("Results: " + output.getMetadata());
+            LOG.info("Results: " + JsonHandler.getInstance().objectToJson(output.getMetadata()));
         } else {
             LOG.info("No data to merge");
         }
