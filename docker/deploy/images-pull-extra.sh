a1e70e568b91ca6df9305eabeac28a679b414ec0
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2017-10-31 08:46:12 +0000

    better specification of docker images

--- docker/deploy/images-pull-extra.sh
+++ docker/deploy/images-pull-extra.sh
@@ -10,6 +10,7 @@ docker pull jboss/keycloak-postgres:2.1.0.Final
 
 docker pull informaticsmatters/rdkit_cartridge:Release_2016_03_1
 docker pull informaticsmatters/rdkit:latest
+docker pull informaticsmatters/rdkit:Release_2017_09_1
 docker pull informaticsmatters/rdkit:Release_2017_03_1
 docker pull informaticsmatters/rdkit_java:latest
 docker pull informaticsmatters/rdkit_java:Release_2017_03_1
@@ -19,6 +20,7 @@ docker pull informaticsmatters/rdkit_pipelines:latest
 docker pull informaticsmatters/rdkit_nextflow:latest
 docker pull informaticsmatters/rdock:latest
 docker pull informaticsmatters/docker_nextflow:latest
+docker pull informaticsmatters/nextflow
 
 docker pull abradle/standardiser
 docker pull abradle/smog2016
