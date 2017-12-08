f245e6053c845dedbdbd31002edb7e855cfc734a
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2017-12-08 16:11:22 +0000

    pv fixes

--- openshift/templates/setenv-template.sh
+++ openshift/templates/setenv-template.sh
@@ -1,12 +1,8 @@
 #@IgnoreInspection BashAddShebang
 
-# You must define these...
+
 export OC_MASTER_HOSTNAME=example.org
-
-# You probably should change these:
 export OC_CERTS_PASSWORD=changeme
-
-# You might want to adjust these...
 export OC_DEPLOYMENT=squonk
 export OC_ADMIN=admin
 export OC_USER=developer
