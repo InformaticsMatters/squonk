4df1493bba22d47a32600b160303c1a2ad762f0e
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2018-01-03 15:49:50 +0000

    fewer images pulled by default

--- docker/deploy/images-pull-extra.sh
+++ docker/deploy/images-pull-extra.sh
@@ -4,18 +4,13 @@ docker pull informaticsmatters/groovy
 
 docker pull rabbitmq:3-management
 docker pull nginx:1.13
-docker pull tomcat:8.0-jre8
-docker pull tomcat:7-jre8
 docker pull jboss/keycloak-postgres:2.1.0.Final
 
-docker pull informaticsmatters/rdkit_cartridge:Release_2016_03_1
+docker pull informaticsmatters/tomcat:8.0-jre8
+docker pull informaticsmatters/rdkit_cartridge:Release_2017_09_2
 docker pull informaticsmatters/rdkit:latest
 docker pull informaticsmatters/rdkit:Release_2017_09_1
 docker pull informaticsmatters/rdkit:Release_2017_03_1
-docker pull informaticsmatters/rdkit_java:latest
-docker pull informaticsmatters/rdkit_java:Release_2017_03_1
-docker pull informaticsmatters/rdkit_java_tomcat:latest
-docker pull informaticsmatters/rdkit_java_tomcat:Release_2017_03_1
 docker pull informaticsmatters/rdkit_pipelines:latest
 docker pull informaticsmatters/rdkit_nextflow:latest
 docker pull informaticsmatters/rdock:latest
