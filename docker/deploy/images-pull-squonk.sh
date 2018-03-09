a65e6125d1d0614f0d0dfa59e7f84789aea50c29
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2018-03-09 09:39:09 +0000

    fixes for docker pull/push

--- docker/deploy/images-pull-squonk.sh
+++ docker/deploy/images-pull-squonk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker pull squonk/chemservices-basic:latest
-docker pull squonk/core-services-server:latest
-docker pull squonk/cellexecutor:latest
 docker pull squonk/portal:latest
+docker pull squonk/chemservices-basic:latest
+docker pull squonk/coreservices:latest
+docker pull squonk/cellexecutor:latest
+docker pull squonk/xwiki:latest
+docker pull squonk/flyway:latest
--- docker/deploy/images-push.sh
+++ docker/deploy/images-push.sh
@@ -6,7 +6,7 @@ base=$PWD
 
 docker push squonk/portal:latest
 docker push squonk/chemservices-basic:latest
-docker push squonk/coreserver:latest
+docker push squonk/coreservices:latest
 docker push squonk/cellexecutor:latest
 docker push squonk/xwiki:latest
 docker push squonk/flyway:latest
