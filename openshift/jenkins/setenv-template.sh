b5239ae293b2ffeac3df92ab473a85bc5ad93ba8
Alan Christie <alan.christie@matildapeak.com>
2018-06-21 15:05:49 +0100

    - Adds experimental jenkins configuration   Depends on use of im-jenkins-utils module

---
+++ openshift/jenkins/setenv-template.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export SQUONK_JENKINS_URL=jenkins-im.apps.xchem.diamond.ac.uk
+export SQUONK_JENKINS_USER=jenkins-admin
+export SQUONK_JENKINS_USER_TOKEN=FixMe
+
+export SQUONK_CXN_MAVEN_USER=FixMe
+export SQUONK_CXN_MAVEN_PASSWORD=FixMe
+
+export PYTHONHTTPSVERIFY=0
