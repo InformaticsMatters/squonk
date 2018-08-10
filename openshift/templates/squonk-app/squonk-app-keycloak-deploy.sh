f02f4dce43ddc2909dd847a90fe8f7bf7dd99ae0
Alan Christie <alan.christie@matildapeak.com>
2018-08-10 15:00:20 +0100

    - Minor tweaks

--- openshift/templates/squonk-app/squonk-app-keycloak-deploy.sh
+++ openshift/templates/squonk-app/squonk-app-keycloak-deploy.sh
@@ -17,7 +17,6 @@ oc process -f squonk-app-keycloak.yaml\
   -p CORE_IMAGE_TAG=$OC_SQUONK_IMAGE_TAG\
   -p CELL_IMAGE_TAG=$OC_SQUONK_IMAGE_TAG\
   -p JE_IMAGE_TAG=$OC_SQUONK_IMAGE_TAG\
-  -p SD_POSTER_IMAGE_TAG=$OC_SQUONK_SD_POSTER_IMAGE_TAG\
   | oc create -f -
 
 echo "Squonk deployment is underway. Once complete it can be accessed at http://${OC_SQUONK_HOST}/portal"
