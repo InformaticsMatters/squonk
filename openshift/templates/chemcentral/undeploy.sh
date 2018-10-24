0e39774d06377c10cfee0ad30a2c3876dd6781d7
Alan Christie <alan.christie@matildapeak.com>
2018-10-24 17:04:57 +0100

    - Adds deprecation notice to existing shell-scripts

--- openshift/templates/chemcentral/undeploy.sh
+++ openshift/templates/chemcentral/undeploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# DEPRECATION NOTICE
+#
+# YOU SHOULD BE USING THE ANSIBLE PLAYBOOKS in openshift/ansible
+# WHERE YOU WILL ALSO FIND A SIMPLE README. ALTHOUGH EVERY ATTEMPT HAS BEEN
+# MADE TO KEEP THE SCRIPT YOU SEE HERE IN GOOD ORDER IT MIGHT BE OUT OF DATE.
+# IF THE EXISTING ANSIBLE PLAYBOOKS ARE NOT SUITABLE MAKE THEM SO!
+
 echo "Deleting chemcentral from $OC_PROJECT project"
 oc delete all,cm,secret --selector template=chemcentral -n $OC_PROJECT
 echo "Deleting chemcentral from $OC_INFRA_PROJECT project"
