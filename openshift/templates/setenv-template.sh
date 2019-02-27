0ff08c2fd6f3423ffeae20a3e02121693fc3dd61
Alan Christie <alan.christie@matildapeak.com>
2019-02-27 14:40:10 +0000

    - Significant rework of templates   All templates are now in the files directory of the corresponding role   t_dir no longer needed - Updated main template - Updated with latest ORD dev environment variables (vault)

--- openshift/templates/setenv-template.sh
+++ openshift/templates/setenv-template.sh
@@ -3,7 +3,7 @@
 # CAUTION:  IF YOU ADD/CHANGE/REMOVE VARIABLES
 #           YOU MUST UPDATE ANY EXAMPLE FILES
 #
-#           i.e. setenv-minishfit.sh
+#           i.e. setenv-minishift.sh
 
 # For an openshift deployment the OC_MASTER_HOSTNAME must be a public domain
 # that will resolve to the master. For minishift this will be the
@@ -23,11 +23,13 @@ export OC_USER_PASSWORD=developer
 
 # For openshift change this to the hostname of the infra node hosting the router
 export OC_ROUTES_BASENAME=${OC_MASTER_HOSTNAME}
+
 export KEYCLOAK_SERVER_URL=https://sso.${OC_ROUTES_BASENAME}/auth
 export KEYCLOAK_REALM=squonk
 export KEYCLOAK_SSL_REQUIRED=external
 export KEYCLOAK_INSECURE_ROUTE=None
 export KEYCLOAK_LOGOUT_REDIRECT_TO=http://home.${OC_ROUTES_BASENAME}/
+
 export OC_INFRA_PROJECT=squonk-infra
 export OC_INFRA_PROJECT_DISPLAY_NAME="Squonk Infrastructure"
 export OC_INFRA_HOURLY_BACKUP_COUNT=7
@@ -36,8 +38,9 @@ export OC_INFRA_DAILY_BACKUP_COUNT=0
 export OC_INFRA_DAILY_BACKUP_SCHEDULE="37 3 * * *"
 export OC_INFRA_BACKUP_VOLUME_SIZE=125Gi
 export OC_INFRA_SA=squonk
+
 export OC_POSTGRESQL_SERVICE=db-postgresql
-export OC_POSTGRESQL_VOLUME_SIZE=125Gi
+
 export OC_PROJECT=squonk
 export OC_PROJECT_DISPLAY_NAME="Squonk Applications"
 export OC_PROJECT_SA=squonk
@@ -45,7 +48,6 @@ export OC_SQUONK_APP=squonk-notebook
 export OC_SQUONK_HOST=${OC_SQUONK_APP}.${OC_ROUTES_BASENAME}
 export OC_SQUONK_CLIENT_SECRET=squonk1234
 export OC_JOBEXECUTOR_HOST=jobexecutor.${OC_ROUTES_BASENAME}
-export OC_SQUONK_CC_PVC_SIZE=20Gi
 
 export OC_SQUONK_CELL_IMAGE_TAG=latest
 export OC_SQUONK_CHEM_IMAGE_TAG=latest
@@ -55,24 +57,24 @@ export OC_SQUONK_PORTAL_IMAGE_TAG=latest
 export OC_SQUONK_SEARCH_IMAGE_TAG=latest
 export OC_SQUONK_FLYWAY_IMAGE_TAG=latest
 
+export OC_POSTGRESQL_VOLUME_SIZE=125Gi
+export OC_CELL_WORK_PVC_SIZE=5Gi
+export OC_CHEMCENTRAL_POSTGRESQL_VOLUME_SIZE=300Gi
+export OC_CHEMCENTRAL_LOADER_VOLUME_SIZE=5Gi
+
 export OC_PIPELINES_SD_POSTER_IMAGE_TAG=latest
 export OC_SQUONK_NEXTFLOW_IMAGE="informaticsmatters/nextflow:18.10.1"
 
 export OC_DOMAIN_NAME=novalocal
 export OC_NFS_SERVER=squonk-infra.$OC_DOMAIN_NAME
+
 export OC_NFS_PATH=/exports
 export OC_SQUONK_VOLUME_TYPE=nfs
 export OC_SQUONK_VOLUME_STORAGE_CLASS=SetIfUsingDynamicVolumeType
 export OC_INFRA_VOLUME_TYPE=nfs
 export OC_INFRA_VOLUME_STORAGE_CLASS=SetIfUsingDynamicVolumeType
-export OC_CHEMCENTRAL_POSTGRESQL_VOLUME_SIZE=300Gi
-export OC_CHEMCENTRAL_LOADER_VOLUME_SIZE=5Gi
 export OC_CHEMCENTRAL_VOLUME_TYPE=nfs
 export OC_CHEMCENTRAL_VOLUME_STORAGE_CLASS=SetIfUsingDynamicVolumeType
-# The path, relative to the directory holding this file,
-# of the working copy of the Squonk pipelines repository.
-# If you don't have a working copy this should be blank.
-export SQUONK_PIPELINES_PATH=../../../pipelines
 
 # Don't really like the NoCows stuff in ansible...
 export ANSIBLE_NOCOWS=1
