c607c8d8ecf0f3639e5f8abcdc2dc5133035ae36
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2019-04-03 14:48:29 +0100

    removed jobexecutor and swagger from dockerised deployment

--- docker/deploy/containers-setup-app.sh
+++ docker/deploy/containers-setup-app.sh
@@ -26,11 +26,6 @@ if [ ! -f images/cellexecutor/logging.properties ]; then
 	cp images/cellexecutor/logging.properties.default images/cellexecutor/logging.properties
 fi
 
-if [ ! -f images/jobexecutor/logging.properties ]; then
-	echo "Creating default logging config for jobexecutor"
-	cp images/cellexecutor/logging.properties.default images/jobexecutor/logging.properties
-fi
-
 if [ ! -f images/portal/logging.properties ]; then
 	echo "Creating default logging config for portal"
 	cp images/portal/logging.properties.default images/portal/logging.properties
@@ -40,17 +35,16 @@ echo "Setting up for server private:${PRIVATE_HOST} public:${PUBLIC_HOST}"
 
 # set up the proxy details in the tomcat apps 
 sed "s/__public_host__/${PUBLIC_HOST}/g" images/portal/server.xml.template > images/portal/server.xml
-sed "s/__public_host__/${PUBLIC_HOST}/g" images/jobexecutor/server.xml.template > images/jobexecutor/server.xml
 
 if [ $DEPLOYMENT_MODE == 'basic' ]; then
-    docker-compose stop   nginx portal chemservices coreservices cellexecutor jobexecutor chemcentral-search swagger
-    docker-compose rm -fv nginx portal chemservices coreservices cellexecutor jobexecutor chemcentral-search swagger
+    docker-compose stop   nginx portal chemservices coreservices cellexecutor chemcentral-search
+    docker-compose rm -fv nginx portal chemservices coreservices cellexecutor chemcentral-search
 elif [ $DEPLOYMENT_MODE == 'site' ]; then
-    docker-compose stop   nginx portal chemservices coreservices cellexecutor jobexecutor chemcentral-search
-    docker-compose rm -fv nginx portal chemservices coreservices cellexecutor jobexecutor chemcentral-search
+    docker-compose stop   nginx portal chemservices coreservices cellexecutor chemcentral-search
+    docker-compose rm -fv nginx portal chemservices coreservices cellexecutor chemcentral-search
 elif [ $DEPLOYMENT_MODE == 'dev' ]; then
-    docker-compose stop   portal chemservices coreservices cellexecutor jobexecutor chemcentral-search
-    docker-compose rm -fv portal chemservices coreservices cellexecutor jobexecutor chemcentral-search
+    docker-compose stop   portal chemservices coreservices cellexecutor chemcentral-search
+    docker-compose rm -fv portal chemservices coreservices cellexecutor chemcentral-search
 else
     echo "ERROR: Must define DEPLOYMENT_MODE to be one of basic, site or dev"
     return
@@ -61,6 +55,3 @@ docker-compose up -d --no-recreate
 
 echo "Setup complete."
 echo "Access squonk at $SQUONK_URL"
-if [ $DEPLOYMENT_MODE == 'basic' ]; then
-    echo "Access swagger at $SWAGGER_URL"
-fi
