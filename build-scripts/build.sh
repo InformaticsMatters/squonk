5cf0c8f37e3807228662790d278b02e9d4969d1a
Alan Christie <alan.christie@matildapeak.com>
2019-08-10 12:03:12 +0100

    - Hack so travis builds ':travis' images   (to avoid disrupting :latest for now)

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,5 +3,18 @@
 # A build script used by Travis
 cd "$TRAVIS_BUILD_DIR"/components || exit
 
-# Build the CellExecutor image
+# Build the application targets
+#
+# squonk/chemcentral-search
+# squonk/coreservices
+# squonk/cellexecutor
+# squonk/jobexecutor
+# squonk/chemcentral-loader
+# squonk/flyway
+
+./gradlew chem-services-rdkit-search:build
+./gradlew core-services-server:build
 ./gradlew cell-executor:build
+./gradlew job-executor:build
+./gradlew rdkit-databases:build
+./gradlew database:build
--- build-scripts/push.sh
+++ build-scripts/push.sh
@@ -10,12 +10,29 @@ cd "$TRAVIS_BUILD_DIR"/components || exit
 # So, to avoid this, we utilise our SQUONK_IMAGE_TAG variable -
 # if this is not a tagged build then set it to 'latest'
 # otherwise copy the tag value.
-export SQUONK_IMAGE_TAG="${TRAVIS_TAG:-latest}"
+export SQUONK_IMAGE_TAG="${TRAVIS_TAG:-travis}"
 
 # Login to docker.io
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
 
-# Construct the CellExecutor image and push...
+# Construct the application docker images
+# and push to docker.io. This wil either be a tag
+# or 'latest'
+
+./gradlew chem-services-rdkit-search:dockerBuildImage -x test
+docker push squonk/chemcentral-search:"${SQUONK_IMAGE_TAG}"
+
+./gradlew core-services-server:dockerBuildImage -x test
+docker push squonk/coreservices:"${SQUONK_IMAGE_TAG}"
+
 ./gradlew cell-executor:dockerBuildImage -x test
-docker images
 docker push squonk/cellexecutor:"${SQUONK_IMAGE_TAG}"
+
+./gradlew job-executor:dockerBuildImage -x test
+docker push squonk/jobexecutor-keycloak:"${SQUONK_IMAGE_TAG}"
+
+./gradlew rdkit-databases:dockerBuildImage -x test
+docker push squonk/chemcentral-loader:"${SQUONK_IMAGE_TAG}"
+
+./gradlew database:dockerBuildImage -x test
+docker push squonk/flyway:"${SQUONK_IMAGE_TAG}"
