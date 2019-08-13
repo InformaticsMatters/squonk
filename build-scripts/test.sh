9d357b5aabcf24b5fbba8d1c513835851bcc362b
Alan Christie <alan.christie@matildapeak.com>
2019-08-13 16:36:32 +0100

    - Tweak test script

--- build-scripts/test.sh
+++ build-scripts/test.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
 # A build script used by Travis
-cd "$TRAVIS_BUILD_DIR"/components || exit
 
 # Do ChemAxon files exist? Some tests rely on these files.
-# If noit presetn we'll set a corresponding environment variable.
-# The files are: -
-# - license.cxl (expected in data/licenses)
-# - chemaxon_reaction_library.zip (expected in docker/deploy/images/chemservices)
-if [[ ! -f ../data/licenses/license.cxl ]]
-then
+# If not present we'll set a corresponding environment variable.
+if [[ ! -f "$TRAVIS_BUILD_DIR"/data/licenses/license.cxl ]]; then
   export CHEMAXON_LICENCE_ABSENT=yes
 fi
-if [[ ! -f ../docker/deploy/images/chemservices/chemaxon_reaction_library.zip ]]
-then
+if [[ ! -f "$TRAVIS_BUILD_DIR"/docker/deploy/images/chemservices/chemaxon_reaction_library.zip ]]; then
   export CHEMAXON_LIBRARY_ABSENT=yes
 fi
 
-# Begin testing...
-
+cd "$TRAVIS_BUILD_DIR"/components || exit
 ./gradlew test
